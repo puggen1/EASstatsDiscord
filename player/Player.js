711c94cb4175bd7125aa5ad16a826ec6cb67de7f
FlyingLawnmower <43915072+puggen1@users.noreply.github.com>
2022-10-26 13:16:22 +0200

    fixed bug with some api elements having space in url

--- player/Player.js
+++ player/Player.js
@@ -7,8 +7,10 @@ class Player {
         this.rankImg = player.global.rank.rankImg;
         this.rankDivision = player.global.rank.rankDiv;
         this.selectedLegend = player.legends.selected.LegendName;
-        this.selectedLegendIcon = player.legends.selected.ImgAssets.icon;
-        this.selectedLegendBanner = player.legends.selected.ImgAssets.banner;
+    
+        this.selectedLegendIcon = player.legends.selected.ImgAssets.icon.includes(" ") ? player.legends.selected.ImgAssets.icon.replace(" ", "%20") : player.legends.selected.ImgAssets.icon; 
+        this.selectedLegendBanner = player.legends.selected.ImgAssets.banner.includes(" ") ? player.legends.selected.ImgAssets.banner.replace(" ", "%20") : player.legends.selected.ImgAssets.banner; 
+
     }
    
 }
